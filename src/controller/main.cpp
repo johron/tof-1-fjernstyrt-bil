f8407e1a9a19034da0da1095d44bd1196b979490
Johan Rong <59508244+johron@users.noreply.github.com>
2024-10-25 17:29:13 +0200

    init

---
+++ src/controller/main.cpp
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#include "OneButton.h"
+#include <IRremote.hpp>
+
+int vrx_pin = A0;
+int vry_pin = A1;
+
+int ir_led_pin = 2;
+int joystick_btn_pin = 3;
+
+bool sent_middle_last = false;
+
+enum Commands {
+    HEX_MIDDLE = 0xBE489,
+    HEX_BRAKE = 0xAB34D,
+    HEX_DRIVE = 0xE4C24,
+    HEX_TURN = 0x2CD5A,
+};
+
+OneButton JoystickButton(joystick_btn_pin, true, true);
+
+void sendIR(unsigned long hex) {
+    unsigned long newhex = hex;
+    if ((newhex & 0xFFF00000) == 0) { // Check if the hex code has less than 32 bits
+        newhex = (newhex << 12) | 0xFFF; // Append FFF to the end
+    }
+    IrSender.sendNECMSB(newhex, 32);
+    delay(10);
+}
+
+void joystick() {
+    int xValue = analogRead(vrx_pin); 
+    int yValue = analogRead(vry_pin);
+    
+    if (xValue < 500 || xValue > 550) { // Speed
+        sent_middle_last = false;
+        unsigned long command = (HEX_DRIVE << 12) | (xValue & 0xFFF);
+        sendIR(command);
+    }
+
+    if (yValue < 500 || yValue > 550) { // Turn
+        sent_middle_last = false;
+        unsigned long command = (HEX_TURN << 12) | (yValue & 0xFFF);
+        sendIR(command);
+    }
+
+    if (!sent_middle_last) {
+        sent_middle_last = true;
+        if (xValue >= 500 && xValue <= 550) {
+            sendIR(HEX_MIDDLE);
+        } else if (yValue >= 500 && yValue <= 550) {
+            sendIR(HEX_MIDDLE);
+        }
+    }
+    
+    delay(10);
+}
+
+static void clickJoystickButton() {
+    sendIR(HEX_BRAKE);
+}
+
+void buttons() {
+    JoystickButton.attachClick(clickJoystickButton);
+    JoystickButton.tick();
+}
+
+void loop() {
+    joystick();
+    buttons();
+}
+
+void setup() {
+    Serial.begin(9600);
+    IrSender.begin(ir_led_pin);
+}
