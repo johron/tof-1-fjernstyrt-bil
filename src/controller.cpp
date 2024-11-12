8bdd3012061b96118cce7447eeb2f4941fcaa196
Johan Rong <59508244+johron@users.noreply.github.com>
2024-11-12 14:06:50 +0100

    Trying to make the sending more efficient

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <CircularBuffer.hpp>
 #include <IRremote.hpp>
 #include <OneButton.h>
 #include <shared.h>
@@ -12,6 +13,8 @@ int joystick_btn_pin = 3;
 bool sent_middlex_last = false;
 bool sent_middley_last = false;
 
+CircularBuffer<long, 15> sentLast;
+
 OneButton JoystickButton(joystick_btn_pin, true, true);
 
 void sendIR(unsigned long hex) {
@@ -19,6 +22,19 @@ void sendIR(unsigned long hex) {
     if ((newhex & 0xFFF00000) == 0) { // Check if the hex code has less than 32 bits
         newhex = (newhex << 12) | 0xFFF; // Append FFF to the end
     }
+    
+    bool allSame = true;
+    for (int i = 0; i < sentLast.size(); i++) {
+        if (sentLast[i] != newhex) {
+            allSame = false;
+            break;
+        }
+    }
+    if (allSame && sentLast.size() > 0) {
+        return;
+    }
+
+    sentLast.push(newhex);
     IrSender.sendNECMSB(newhex, 32);
     delay(10);
 }
@@ -26,7 +42,7 @@ void sendIR(unsigned long hex) {
 void joystick() {
     int xValue = analogRead(vrx_pin); 
     int yValue = analogRead(vry_pin);
-    
+
     if (xValue < 500 || xValue > 550) { // Speed
         sent_middlex_last = false;
         unsigned long command = (HEX_DRIVE << 12) | (xValue & 0xFFF);
@@ -39,19 +55,15 @@ void joystick() {
         sendIR(command);
     }
 
-    //if (!sent_middlex_last) {
-        if (xValue >= 500 && xValue <= 550) {
-            sent_middlex_last = true;
-            sendIR(HEX_MIDDLEX);
-        }
-    //}
+    if (xValue >= 500 && xValue <= 550) {
+        sent_middlex_last = true;
+        sendIR(HEX_MIDDLEX);
+    }
 
-    //if (!sent_middley_last) {
-        if (yValue >= 500 && yValue <= 550) {
-            sent_middley_last = true;
-            sendIR(HEX_MIDDLEY);
-        }
-    //}
+    if (yValue >= 500 && yValue <= 550) {
+        sent_middley_last = true;
+        sendIR(HEX_MIDDLEY);
+    }
     
     delay(10);
 }
