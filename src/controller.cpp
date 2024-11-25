31986716d4ad7e0b113f8ad06dd2c33f58c972eb
Johan Rong <59508244+johron@users.noreply.github.com>
2024-11-25 21:30:32 +0100

    Add comments to controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,9 +3,9 @@
 #include <OneButton.h>
 #include <shared.h>
 
-int vry_pin = A0;
+int vry_pin = A0; /* In this project i use the X axis as the throttle because that's
+                     how i prefer to hold it, so X is vertical and Y is horizontal */
 int vrx_pin = A1;
-
 int ir_led_pin = 2;
 int joystick_btn_pin = 3;
 
@@ -15,15 +15,21 @@ unsigned long lastCommandTime = 0;
 unsigned long lastCommand = 0xF00FF00F;
 unsigned long delayTime = 1000;
 
+// Function to send IR command
 void sendIR(unsigned long hex) {
-    unsigned long currentTime = millis();
+    unsigned long currentTime = millis(); // Get the current time
     unsigned long newhex = hex;
+
+    // Check if the command is different or if the delay time has passed
     if (newhex != lastCommand || (currentTime - lastCommandTime) >= delayTime) {
+        // Check if the hex number is eight digits long
         if ((newhex & 0xFFF00000) == 0) {
-            newhex = (newhex << 12) | 0xFFF;
+            newhex = (newhex << 12) | 0xFFF; /* Add three hexadecimal digits at the end FFF (dec:4095)
+                                                so the number is eight digits long */
         }
 
-        IrSender.sendNECMSB(newhex, 32);
+        IrSender.sendNECMSB(newhex, 32); /* 32 because it is a eight digit hexadecimal
+                                            number which is 32 bits 8*16=32 */
         delay(10);
 
         lastCommand = newhex;
@@ -32,23 +38,28 @@ void sendIR(unsigned long hex) {
 }
 
 void joystick() {
-    int xValue = analogRead(vrx_pin); 
+    int xValue = analogRead(vrx_pin);
     int yValue = analogRead(vry_pin);
 
+    // Check if the joystick is moved in the x direction
     if (xValue < 450 || xValue > 600) {
-        unsigned long command = (HEX_DRIVE << 12) | (xValue & 0xFFF);
+        unsigned long command = (HEX_DRIVE << 12) | (xValue & 0xFFF); /* Add the value in the direction as 3 hexadecimal digits
+                                                                         at the end of the operation hex code */
         sendIR(command);
     }
 
+    // Check if the joystick is moved in the y direction
     if (yValue < 450 || yValue > 600) {
         unsigned long command = (HEX_TURN << 12) | (yValue & 0xFFF);
         sendIR(command);
     }
 
+    // Check if the joystick is centered in the x direction
     if (xValue >= 450 && xValue <= 600) {
         sendIR(HEX_MIDDLEX);
     }
 
+    // Check if the joystick is centered in the y direction
     if (yValue >= 450 && yValue <= 600) {
         sendIR(HEX_MIDDLEY);
     }
@@ -64,7 +75,6 @@ void buttons() {
     JoystickButton.attachClick(clickJoystickButton);
     JoystickButton.tick();
 }
-
 
 void loop() {
     joystick();
