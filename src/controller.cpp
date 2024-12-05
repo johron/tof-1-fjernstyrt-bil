e80e316442d17cbe37be5cba18a8df375d51c54e
Johan Rong <59508244+johron@users.noreply.github.com>
2024-12-05 14:37:43 +0100

    laste opp

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
 #include <OneButton.h>
-#include <shared.hpp>
 
-int vry_pin = A0;
-int vrx_pin = A1;
-int ir_led_pin = 2;
-int joystick_btn_pin = 3;
+#include "shared.hpp"
 
-OneButton JoystickButton(joystick_btn_pin, true, true);
+const int vry_pin = A0;
+const int vrx_pin = A1;
+const int ir_led_pin = 2;
+const int joystick_btn_pin = 3;
 
-unsigned long lastCommandTime = 0;
-unsigned long lastCommand = 0xF00FF00F;
-unsigned long delayTime = 1000;
+unsigned long lastCommands[2];
 
 // Function to send IR command
 void sendIR(unsigned long hex) {
@@ -32,41 +29,33 @@ void sendIR(unsigned long hex) {
 void joystick() {
     int xValue = analogRead(vrx_pin);
     int yValue = analogRead(vry_pin);
+    unsigned long commands[2];
 
-    // Check if the joystick is moved in the x direction
-    if (xValue < 450 || xValue > 600) {
-        unsigned long command = (HEX_DRIVE << 12) | (xValue & 0xFFF); /* Add the value in the direction as 3 hexadecimal digits
-                                                                         at the end of the operation hex code */
-        sendIR(command);
+    if (xValue < 450) {
+        commands[0] = (HEX_DRIVE << 12) | 0x3FF;
+    } else if (xValue > 550) {
+        commands[0] = (HEX_DRIVE << 12) | 0x000;
     }
 
-    // Check if the joystick is moved in the y direction
-    if (yValue < 450 || yValue > 600) {
-        unsigned long command = (HEX_TURN << 12) | (yValue & 0xFFF);
-        sendIR(command);
+    if (yValue < 450) {
+        commands[1] = (HEX_TURN << 12) | 0x3FF;
+    } else if (yValue > 550) {
+        commands[1] = (HEX_TURN << 12) | 0x000;
     }
 
-    // Check if the joystick is centered in the x direction
-    /*if (xValue >= 450 && xValue <= 600) {
-        unsigned long command = (HEX_MIDDLE << 12) | 0x000;
-        sendIR(command);
+    for (int i = 0; i < 2; i++) {
+        if (commands[i] != 0) {
+            if (commands[i] != lastCommands[i]) break;
+            sendIR(commands[i]);
+        }
+        lastCommands[i] = commands[i];
     }
-
-    // Check if the joystick is centered in the y direction
-    if (yValue >= 450 && yValue <= 600) {
-        unsigned long command = (HEX_MIDDLE << 12) | 0xFFF;
-        sendIR(command);
-    }*/
 
     delay(10);
 }
 
-void buttons() {
-}
-
 void loop() {
     joystick();
-    buttons();
 }
 
 void setup() {
